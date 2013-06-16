679ff005950290647ac32823bd3d6d4f2842dbaf
NitroGhost <wildeman.benjamin@gmail.com>
2013-06-16 22:55:19 +0200

    Update manager.js

--- manager.js
+++ manager.js
@@ -811,7 +811,7 @@ delay();
 $('#plugbot-js').remove();
 log("Also, welcome to ★Anime Games Music★, coded by Nitro Ghost. Version: 4.1.4");
 log("type '/commands' to see extra commands");
-$('body').prepend('<script type="text/javascript" id="modcommands-js" src="https://raw.github.com/NitroGhost/DTE/master/modcommands.js" />');
+$('body').prepend('<script type="text/javascript" id="modcommands-js" src="https://raw.github.com/Snipeglider/Plug/master/modcommands.js" />');
 $('body').prepend('<style type="text/css" id="plug-css">' + "\n" + styles.join("\n") + "\n" + '</style>');
 $('body').append('</div><div id="side-right" class="sidebar">' + '<div class="sidebar-handle"><span>|||</span></div>' + '<div class="sidebar-content"></div>' + '<div id="hr-div"><div><div id="hr-style"></div></div></div>' + '</div><div id="side-left" class="sidebar">' + '<div class="sidebar-handle" title="show/hide userlist"><span>|||</span></div>' + '<div class="sidebar-content2"></div>' + '<div id="hr2-div2"><div><div id="hr2-style2"></div></div></div>' + '</div>');
 $('body').append('<script type="text/javascript" id="plug-js-extra">' + "\n" + scripts.join("\n") + "\n" + '</script>');
