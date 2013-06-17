b2986281f9264ff08940dba9ad5549b465aa7cd2
NitroGhost <wildeman.benjamin@gmail.com>
2013-06-17 17:02:07 +0200

    Update autoskip.js

--- autoskip.js
+++ autoskip.js
@@ -9,7 +9,7 @@ function autoSkip() {
 		setTimeout("new ModerationForceSkipService;", 500);
 		setTimeout("new RoomPropsService(document.location.href.split('/')[3],false,true,1,5);", 750);
 	}
-	if (Models.room.data.media.duration > 481) {
+	if (Models.room.data.media.duration > 421) {
 		API.sendChat("/me auto skip activated! song exceeds 8 minutes long");
 		setTimeout("new RoomPropsService(document.location.href.split('/')[3],true,true,1,5);", 250);
 		setTimeout("new ModerationForceSkipService;", 500);
@@ -20,7 +20,7 @@ function autoSkip() {
 function roomSkip() {
 	var tv = Models.room.roomScore.negative + Models.room.roomScore.positive;
 	var tvp = Models.room.roomScore.negative / tv;
-	if(tvp >= 20 && tv >= 45) {
+	if(tvp >= 20) {
 		new ModerationForceSkipService;
 		API.sendChat("room voted to skip!");
 	}
