c3a4780567ebcd61c0993c9c0f0258ab8c0463bb
Snipeglider <chrapovickij@gmail.com>
2013-06-18 00:24:32 +0200

    Update autoskip.js

--- autoskip.js
+++ autoskip.js
@@ -10,7 +10,7 @@ function autoSkip() {
 		setTimeout("new RoomPropsService(document.location.href.split('/')[3],false,true,1,5);", 750);
 	}
 	if (Models.room.data.media.duration > 421) {
-		API.sendChat("/me auto skip activated! song exceeds 8 minutes long");
+		API.sendChat("/me auto skip activated! song exceeds 7 minutes long");
 		setTimeout("new RoomPropsService(document.location.href.split('/')[3],true,true,1,5);", 250);
 		setTimeout("new ModerationForceSkipService;", 500);
 		setTimeout("new RoomPropsService(document.location.href.split('/')[3],false,true,1,5);", 750);
