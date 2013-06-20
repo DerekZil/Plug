443f92550b226b0b56b86f8b4e6add787e1a6079
Snipeglider <chrapovickij@gmail.com>
2013-06-20 17:03:11 +0200

    Update autoskip.js

--- autoskip.js
+++ autoskip.js
@@ -4,13 +4,13 @@ overPlayed = [
 
 function autoSkip() {
 	if (overPlayed.indexOf(Models.room.data.media.id) > -1) {
-		API.sendChat("/me auto skip activated! song overplayed");
+		API.sendChat("/me Auto skip activated! song overplayed.");
 		setTimeout("new RoomPropsService(document.location.href.split('/')[3],true,true,1,5);", 250);
 		setTimeout("new ModerationForceSkipService;", 500);
 		setTimeout("new RoomPropsService(document.location.href.split('/')[3],false,true,1,5);", 750);
 	}
 	if (Models.room.data.media.duration > 421) {
-		API.sendChat("/me auto skip activated! song exceeds 7 minutes long");
+		API.sendChat("/me Auto skip activated! song exceeds 7 minutes long.");
 		setTimeout("new RoomPropsService(document.location.href.split('/')[3],true,true,1,5);", 250);
 		setTimeout("new ModerationForceSkipService;", 500);
 		setTimeout("new RoomPropsService(document.location.href.split('/')[3],false,true,1,5);", 750);
