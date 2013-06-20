32b966c3a6c07b5b1a5bc39baa8bce9efac9d99f
Snipeglider <chrapovickij@gmail.com>
2013-06-20 17:04:01 +0200

    Update blacklist.js

--- blacklist.js
+++ blacklist.js
@@ -6,7 +6,7 @@ API.addEventListener(API.USER_JOIN, checkBlacklist);
 
 function checkBlacklist(user) {
         if (blacklist.indexOf(user.id) > -1 ) {
-                API.sendChat("/me blacklisted user detected!");
+                API.sendChat("/me Blacklisted user detected!");
                 API.moderateBanUser(user.id, "Blacklisted User. banned for 30 days");
         }
 }
