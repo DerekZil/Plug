5c2ef9756d9669cda19c15467c2522977f1bc99f
Derek Zil <DerAndZil@Gmail.com>
2013-07-15 07:26:32 -0400

    Update blacklist.js

--- blacklist.js
+++ blacklist.js
@@ -5,6 +5,6 @@ API.addEventListener(API.USER_JOIN, checkBlacklist);
 function checkBlacklist(user) {
         if (blacklist.indexOf(user.id) > -1 ) {
                 API.sendChat("/me Blacklisted user detected!");
-                API.moderateBanUser(user.id, "Blacklisted User. banned for 30 days");
+                API.moderateBanUser(user.id, "Blacklisted User. banned for 30 days.");
         }
 }
